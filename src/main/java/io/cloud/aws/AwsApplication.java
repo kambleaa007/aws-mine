1ca68fd9855ab4978af4fadcf85559dd0793be8a
kambleaa007 <ashishkamble191191@gmail.com>
2024-02-24 13:39:12 +0530

    First commit

---
+++ src/main/java/io/cloud/aws/AwsApplication.java
@@ -0,0 +1,15 @@
+package io.cloud.aws;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class AwsApplication {
+
+	public static void main(String[] args) {
+
+		run(AwsApplication.class, args);
+	}
+
+}
